465b959445b608db173c3df61d38ed4017a491ad
Dan <danielandrewvoss@gmail.com>
2016-10-16 14:53:11 -0400

    Finish variable scoping

--- src/com/dvoss/Main.java
+++ src/com/dvoss/Main.java
@@ -54,16 +54,18 @@ public class Main {
                     for (String command : commands) {
                         processCommand(scanner, variables, command);
                     }
-
+                    variables.remove(variables.size() - 1);
                 }
                 break;
             case "IF_DAT_BOOTY_KEEP_POPPIN":
                 boolean condition = Boolean.valueOf(params);
                 ArrayList<String> commands2 = readLinesInScope(scanner);
                 if (condition) {
+                    variables.add(new HashMap<>());
                     for (String command : commands2) {
                         processCommand(scanner, variables, command);
                     }
+                    variables.remove(variables.size() - 1);
                 }
                 break;
         }
